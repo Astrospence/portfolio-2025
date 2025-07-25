0d2d4f94804452f8b1f42deb3f9a23d0e70287a3
Astrospence <illustrator.wood@gmail.com>
2025-07-25 16:29:25 -0500

    banner and header updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import styles from "./page.module.css";
 import Header from './header'
+import Banner from './banner'
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <Header />
+        <Banner />
       </main>
       <footer className={styles.footer}>
         
