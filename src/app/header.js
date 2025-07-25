c81d0e698b858387e10b3c6e15545d076ad49151
Astrospence <illustrator.wood@gmail.com>
2025-07-25 13:34:27 -0500

    initialized page and header layout

--- src/app/header.js
+++ src/app/header.js
@@ -1,18 +1,21 @@
-import styles from './header.css'
+import styles from './header.module.css'
 
 export default function Header () {
   return (
     <div className={styles.header}>
       <div className={styles.menu}>
-
+        <div className={styles.menuLine}></div>
+        <div className={styles.menuLine}></div>
+        <div className={styles.menuLine}></div>
       </div>
       <div className={styles.nav}>
         <nav className={styles.links}>
-          <a href=''>Resume</a>
-          <a href=''>About</a>
-          <a href=''>Contact</a>
-          <a><button>Desktop Components</button></a>
-          <a><button>Mobile Components</button></a>
+          <a className={styles.indLinks} href=''>Resume</a>
+          <a className={styles.indLinks} href=''>About</a>
+          <a className={styles.indLinks} href=''>Contact</a>
+          <div className={styles.linkSeparator}></div>
+          <a className={styles.indLinks}><button>Desktop Components</button></a>
+          <a className={styles.indLinks}><button>Mobile Components</button></a>
         </nav>
       </div>
     </div>
