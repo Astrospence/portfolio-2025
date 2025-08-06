e6546a5d55c372ff7fb7fbe03ddcd05a80776a57
Astrospence <illustrator.wood@gmail.com>
2025-08-06 12:02:36 -0500

    developing new banner design

--- src/app/banner.js
+++ src/app/banner.js
@@ -1,18 +1,18 @@
 import styles from './banner.module.css'
 import Image from 'next/image'
 import Headshot from '../../public/SpencerHeadshot.jpg'
+import Network from '../../public/Network.png'
 
 export default function Banner () {
   return (
     <div className={styles.banner}>
       <div className={styles.bannerContent}>
-        <div className={styles.headshotContainer}>
+        <div className={styles.leftContainer}>
           <Image className={styles.headshot} src={Headshot} alt='headshot of Spencer Wood' />
         </div>
-        <h1>SPENCER WOOD</h1>
-        <p>
-          Welcome to my Portfolio Site. I&apos;m eager to showcase a collection of some of my skills, as well as my resume, info about myself, and way to contact me if you&apos;d like to connect. Keep scrolling and we&apos;ll get started!
-        </p>
+        <div className={styles.rightContainer}>
+          <Image className={styles.bannerBackgroundNetwork} src={Network} alt='vector of globe with dotted arrows orbiting' />
+        </div>
       </div>
     </div>
   )
